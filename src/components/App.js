c387598326efe496897decab8e252fd6aa19d3a0
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-05-23 23:47:45 -0300

    History

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import SignIn from "./Login/SignIn";
 import SignUp from "./Login/SignUp";
 import Today from "./Today/Today";
 import Habits from "./Habits/Habits";
+import History from "./History/History";
 
 export default function App() {
   const [user, setUser] = useState();
@@ -24,6 +25,9 @@ export default function App() {
           </Route>
           <Route path="/habitos" exact>
             <Habits></Habits>
+          </Route>
+          <Route path="/historico" exact>
+            <History></History>
           </Route>
         </UserContext.Provider>
       </Switch>
---
+++ src/components/History/History.js
@@ -0,0 +1,11 @@
+import Menu from "../Bars/Menu";
+import Topo from "../Bars/Topo";
+
+export default function () {
+  return (
+    <>
+      <Topo></Topo>
+      <Menu></Menu>
+    </>
+  );
+}
